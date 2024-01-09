4c46c1c949ef8d1f1a4440b90844535c84c57bb1
MuminaB <barrowmuminatou@gmail.com>
2024-01-09 04:32:00 +0000

    update

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,6 @@
+-- Display all databases
+SHOW databases;
 --Create a database if missing
 CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
- 
+-- Delete database hbtn_0c_0
+DROP DATABASE IF EXISTS 'hbtn_0c_0';
