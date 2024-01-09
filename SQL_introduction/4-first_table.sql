fd4aa298ed40bdd5d42a30a8d369c42215888b7c
MuminaB <barrowmuminatou@gmail.com>
2024-01-09 23:09:07 +0000

    changes

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
 -- Script that creates a table called first_table in the current database of MySQL server
-CREATE TABLE `first_table` IF NOT EXISTS(`id` INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table`(
+    `id` INT, 
+    `name` VARCHAR(256)
+    );
  
