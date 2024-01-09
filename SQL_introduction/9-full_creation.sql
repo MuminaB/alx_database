d928d9776988c46df5a611eda55ed619e9b30c91
MuminaB <barrowmuminatou@gmail.com>
2024-01-09 23:37:10 +0000

    backtic

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Script that creates a second table in the database hbtn_0c_0 with multiple rows
-CREATE TABLE IF NOT EXISTS second_table(`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO second_table(`id`, `name`, `score`) VALUES (
+CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES (
     (1, "John", 10),
     (2, "Alex", 3),
     (3, "Bob",  14),
